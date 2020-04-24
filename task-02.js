2766dc5f26585cd4789a54b35ab0589f67ec6dd6
slavik <atayanvr@gmail.com>
2020-04-24 22:01:36 +0300

    added 2 task

---
+++ task-02.js
@@ -0,0 +1,17 @@
+const total = 100;
+let ordered = 50;
+
+if(ordered < total){
+    console.log("Заказ оформлен, с вами свяжется менеджер".)
+} else {
+    console.log("На складе недостаточно твоаров!".)
+};
+
+ordered = 80
+
+if(ordered < total){
+    console.log("Заказ оформлен, с вами свяжется менеджер".)
+} else {
+    console.log("На складе недостаточно твоаров!".)
+};
+
